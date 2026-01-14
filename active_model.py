35a2ecd9a618ec5af072736531b6d1e24c203667
als244 <andrew.sheinberg@yale.edu>
2026-01-13 20:51:46 -0500

    Import error

--- active_model.py
+++ active_model.py
@@ -6,7 +6,7 @@ import os
 import sys
 import gc
 
-from .saved_activations_policy import get_transformer_saved_act_sizes
+from awsm_transformer import get_transformer_saved_act_sizes
 from transmission_scheduler import TransmissionScheduler
 
 _cudart = ctypes.CDLL('libcudart.so')
--- awsm_transformer/__init__.py
+++ awsm_transformer/__init__.py
@@ -7,3 +7,5 @@ from .bench_matmul import bench_matmul
 from .bench_transfer import bench_transfer
 from .hardware_env import get_transformer_transfer_report, get_transformer_matmul_report, get_hardware_env
 from .mem_register import pin_tensor
+from .saved_activations_policy import get_transformer_saved_act_sizes
+from .query_memory import *
