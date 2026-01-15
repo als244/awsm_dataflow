b7c17e14028cec2c422a14aace1123797c880a8d
als244 <andrew.sheinberg@yale.edu>
2026-01-15 05:03:17 -0500

    Updae

--- active_model.py
+++ active_model.py
@@ -519,7 +519,7 @@ class ActiveModel:
         return
 
           
-    def determine_saved_levels(self, seq_groups, verbose=False):
+    def determine_saved_levels(self, seq_groups, verbose=True):
 
         total_chunks = sum([len(seq_group) for seq_group in seq_groups])
         total_round_tokens = 0
